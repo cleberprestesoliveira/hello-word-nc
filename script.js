032eb10627de8decd491ab2f89c3196d9cd74042
cleberprestesoliveira <107960108+cleberprestesoliveira@users.noreply.github.com>
2025-06-24 20:36:30 -0300

    update do script

---
+++ script.js
@@ -0,0 +1,7 @@
+function inseriNome() {
+    let nomeUsuario = prompt("Qual o seu nome?");
+    let elemento = document.querySelector("#nome-usuario");
+    elemento.textContent = nomeUsuario;    
+}
+
+inseriNome();
